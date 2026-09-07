7077cfcec78bda71aa18ac079e6dc5996ba47b81
Manqoba-123 <manoba231@gmail.com>
2026-09-07 11:11:26 +0200

    setting up the exercise page

--- fitness-tracker/src/App.jsx
+++ fitness-tracker/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navigation/Navbar.jsx';
 import Home from './pages/Home.jsx';
 
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* We will add /exercises, /workout-planner, etc. routes next */}
+        {/* Placeholder routes for next steps */}
+        <Route path="/exercises" element={<div style={{ padding: '3rem', textAlign: 'center', color: '#fff' }}>Exercises Page Coming Next!</div>} />
+        <Route path="/workout-planner" element={<div style={{ padding: '3rem', textAlign: 'center', color: '#fff' }}>Weekly Planner Page Coming Soon!</div>} />
       </Routes>
     </div>
   );
